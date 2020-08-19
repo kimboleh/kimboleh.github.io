1e49b35b546d09b8a4f6fa38bdedaf21ebfeae5f
Horton, Kimberly <kimberly.horton@tektronix.com>
2020-08-18 18:38:04 -0700

    Updates

---
+++ precache-manifest.1cd34f3482fe90624491e0b3905f7e40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c30bbef4e32bb2a86a83f5d824b471",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "730d35394db644787d8c",
+    "url": "/react-portfolio/static/css/main.e7d00a0c.chunk.css"
+  },
+  {
+    "revision": "82ae99bbb3e0e22547f4",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730d35394db644787d8c",
+    "url": "/react-portfolio/static/js/main.cae02f5d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff656bbf69c0fd9d25194d4a7d611be2.js"
+  "/react-portfolio/precache-manifest.1cd34f3482fe90624491e0b3905f7e40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
