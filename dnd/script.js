59d7b8c751ddb2497693aabac55e9a686174f717
k-horton <khorton17@georgefox.edu>
2020-11-06 00:32:19 -0800

    spells appear now!

--- dnd/script.js
+++ dnd/script.js
@@ -27,9 +27,16 @@ $(document).ready(function () {
   });
 
   $('#submitAll').click(function() {
-    $('#onboarding-content').css("display", "none");
-    // todo: get Spells
+    $('input[type=checkbox]:checked').each(function () {
+      for(spell of spells) {
+        if (JSON.parse(spell).index == this.value) {
+          $('#charSpells').append(JSON.parse(spell).name + "<br>" + JSON.parse(spell).desc + "<br>");
+        }
+        // console.log(JSON.parse(spell).desc);
+      }
+    });
 
+    $('#onboarding-content').css("display", "none");
     $('#characterContent').css("display", "grid");
   });
 
@@ -71,26 +78,27 @@ jQuery.fn.getSpells = function () {
       url: "https://www.dnd5eapi.co/api/spells?level=" + lvlParam,
       success: function(resp) {
         for (spell of resp.results) {
-          $.fn.getSpell(spell);
+          $.fn.getClassSpells(spell);
         }
       },
       error: function() {}
     });
   };
 
-jQuery.fn.getSpell = function (data) {
+jQuery.fn.getClassSpells = function (data) {
     $.ajax({
         type: 'GET',
         url: "https://www.dnd5eapi.co" + data.url,
         success: function(resp) {
+          console.log("resp: " + resp);
           for (_class of resp.classes) {
             if (_class.name == charClass) {
-              console.log(_class.name + " can cast " + data.name);
+              spells.push(JSON.stringify(resp));
               $('#getSpells').append(
-                "<li><input type='checkbox' id=" +
-                  data.name + " name=" + data.name + " value=" +
-                  data + " /> <label for=" + data.name + ">" + data.name +
-                  "</label></li>"
+                '<li><input type="checkbox" id=' +
+                  data.name + ' name=' + data.name + ' value=' +
+                  data.index + ' /> <label for=' + data.name + '>' + data.name +
+                  '</label></li>'
               );
             }
           }
