ee510bad49ed6f37c61a05ccec1c97f41c0413ea
Kimboleh <kimberlynachbur@gmail.com>
2026-03-06 19:29:43 -0800

    create sample homepage and js layout

---
+++ src/js/layout.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // insert header
+    const pageBody = document.querySelector("body");
+
+    const bannerHeader = document.createElement("div");
+    bannerHeader.id = "header";
+    const siteTitle = document.createElement("h1");
+    siteTitle.textContent = "Kimbo's Site";
+    const siteSubtitle = document.createElement("h2");
+    siteSubtitle.textContent = "Welcome to my site!";
+
+    bannerHeader.appendChild(siteTitle);
+    bannerHeader.appendChild(siteSubtitle);
+    pageBody.insertBefore(bannerHeader, pageBody.firstChild);
+
+    // insert menus
+    const mainContainer = document.querySelector("#container");
+
+    const leftMenu = document.createElement("div");
+    leftMenu.id = "left-menu";
+    leftMenu.classList.add("nav-menu");
+    const rightMenu = document.createElement("div");
+    rightMenu.id = "right-menu";
+    const rightLinks = document.createElement("div");
+    rightLinks.id = "right-links"
+    rightLinks.classList = "window";
+    const statusCafe = document.createElement("div");
+    statusCafe.id = "statuscafe";
+    statusCafe.classList = "window";
+    statusCafe.innerHTML = `<div id="statuscafe-username"></div><div id="statuscafe-content"></div>`;
+    rightMenu.appendChild(statusCafe);
+    rightMenu.appendChild(rightLinks);
+    mainContainer.appendChild(leftMenu);
+    mainContainer.appendChild(rightMenu);
+    getStatusCafe();
+
+    const leftLinks = document.createElement("ul");
+
+    // asynchronously fetch the site pages and then add them to the list
+    fetch("../src/site-pages.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            for (page in data) {
+                let listItem = document.createElement("li");
+                let link = document.createElement("a");
+                link.textContent = page;
+                link.href = data[page].url;
+                listItem.appendChild(link);
+                leftLinks.appendChild(listItem);
+            }
+        })
+        .catch(error => console.error('Failed to fetch data:', error));
+
+    leftMenu.appendChild(leftLinks);
+
+    rightLinks.innerHTML =
+        `
+            <ul>
+                <li>Meet My Neighbors!</li>
+                    <ul>
+                        <li><a title="olorollo" href="https://olorollo.neocities.org/" target="_blank"><img src="https://olorollo.neocities.org/olorollo-btn.gif"></a></li>
+                    </ul>
+                <li>Cool Sites</li>
+                    <ul>
+                        <li><a href="https://www.dragonflycave.com"><img src="../src/img/buttons/tcod.gif" /></a></li>
+                        <li><a href="https://libre.town/"><img src="../src/img/buttons/8834_libretown.gif" /></a></li>
+                        <li><a href="https://www.thefrugalgamer.net/"><img src="../src/img/buttons/frugalgamer_button2.png" /></a></li>
+                        <li><a href="https://32bit.cafe/"><img src="../src/img/buttons/32bitty-loren.png" /></a></li>
+                    </ul>
+                <li>Link my site!</li>
+                <li><img src="../src/img/buttons/kimbositebtn.png" /></li>
+            </ul>
+            <hr>
+            <img src="../src/img/buttons/antiai_button.png" />
+        `;
+});
+
+function getStatusCafe() {
+    fetch("https://status.cafe/users/kimbo/status.json")
+        .then( r => r.json() )
+        .then( r => {
+            if (!r.content.length) {
+                document.getElementById("statuscafe-content").innerHTML = "No status yet."
+                return
+            }
+            document.getElementById("statuscafe-username").innerHTML = '<a href="https://status.cafe/users/kimbo" target="_blank">' + r.author + '</a> ' + r.face + ' ' + r.timeAgo
+            document.getElementById("statuscafe-content").innerHTML = r.content
+        })
+}
